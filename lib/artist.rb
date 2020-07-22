deae6854dcc0bf573203300483c352b2b924495b
Ceddyp19 <ceddyp117@gmail.com>
2020-07-22 16:26:49 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+require 'pry'
+class Artist
+   attr_accessor :name 
+
+   @@all = []
+   
+
+   def initialize (name)
+     @name = name 
+     self.class.all << self 
+   end
+
+   def self.all
+    @@all
+   end
+
+   def songs 
+      Song.all.select do |song|
+          song.artist == self
+      end
+   end 
+
+   def add_song(song)
+     
+   end
+
+end 
---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+  attr_accessor :title, :artist 
+
+  @@all = []
+
+    def initialize(title)
+      @title = title
+      self.class.all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+
+end 
