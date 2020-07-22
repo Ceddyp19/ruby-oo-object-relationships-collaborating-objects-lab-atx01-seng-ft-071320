2df001a312869046c73473d557cc223785a0efcb
Ceddyp19 <ceddyp117@gmail.com>
2020-07-22 16:51:03 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,8 +20,28 @@ class Artist
       end
    end 
 
-   def add_song(song)
-     
-   end
+    def add_song(song)
+      song.artist = self 
+    end
+
+    def self.find_or_create_by_name(name)
+      self.find(name) ? self.find(name) : self.new(name)
+    end
+
+    def self.find(name)
+       self.all.find do |artist|
+         artist.name == name 
+       end
+    end
+
+    def save
+        @@all << self
+    end
+
+    def print_songs
+        songs.each do |song|
+            puts song.name 
+        end
+    end
 
 end 
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 class Song
-  attr_accessor :title, :artist 
+  attr_accessor :name, :artist 
 
   @@all = []
 
-    def initialize(title)
-      @title = title
+    def initialize(name)
+      @name = name
       self.class.all << self
     end
 
@@ -12,5 +12,20 @@ class Song
       @@all
     end
 
+    def save
+        @@all << self
+    end
+
+    def self.new_by_filename(filename)
+        artist, song = filename.split(" - ")
+        new_song = self.new(song)
+        new_song.artist_name = artist
+        new_song
+    end
+
+    def artist_name=(name)
+        self.artist = Artist.find_or_create_by_name(name)
+        artist.add_song(self) 
+    end
 
 end 
